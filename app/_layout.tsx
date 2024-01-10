d3429259dcc5001a5e06bfb664c503445551192f
Artem0328 <laoroney1113@gmail.com>
2024-01-10 23:42:05 +0530

    push notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,11 @@ import LoginScreen from "./Login";
 
 import useBasketStore from "@/store/basketStore";
 import QueryClient from "@/core/lib/QueryClient";
+
+import * as Device from "expo-device";
+import { Platform } from "react-native";
+
+import * as Notifications from "expo-notifications";
 import { setAuthHeader } from "@/core/lib/AxiosClient";
 
 export {
@@ -94,8 +99,64 @@ export default function RootLayout() {
   return token ? <RootLayoutNav /> : <LoginScreenWithQueryClient />;
 }
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 function RootLayoutNav() {
   const navigation = useNavigation();
+  // Notification Setup
+  useEffect(() => {
+    console.log("Registering for push notifications...");
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log("token: ", token);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      console.log(existingStatus);
+
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: "42cdfbfe-dd38-4312-98bd-32808f5c81cc",
+        })
+      ).data;
+      console.log(token);
+    } else {
+      alert("Must use a physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
   return (
     <QueryClient>
       <BottomSheetModalProvider>
